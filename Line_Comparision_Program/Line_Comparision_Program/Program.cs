d0e70f904f1de14a12da332a4ff93bdd11351b8f
remant <remantmahato9798@gmail.com>
2022-03-07 22:47:50 +0530

    added : UC1-LengthOfLine

--- Line_Comparision_Program/Line_Comparision_Program/Program.cs
+++ Line_Comparision_Program/Line_Comparision_Program/Program.cs
@@ -1,4 +1,32 @@
-﻿Console.WriteLine("Welcome to line comparision program");
+﻿
+using System;
+namespace Line_Comparision_Program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome the Line Comparision program \n");
+            Console.WriteLine("Enter the vaues of coordinates \n");
+            Console.WriteLine("Enter the value of x1:");
+            int x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2:");
+            int x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y1:");
+            int y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y2:");
+            int y2 = int.Parse(Console.ReadLine());
+
+            double Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of Line :" + Length);
+
+        }
+
+    }
+}
 
 
 
