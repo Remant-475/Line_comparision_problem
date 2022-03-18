00837f5f2a059ecb2e3f727fea44a04e663e119d
Remant <remantmahato9798@gmail.com>
2022-03-18 23:51:36 +0530

    Added : UC4-Line_Comparision_Using_Method

--- Line_Comparision_Program/Line_Comparision_Program/Program.cs
+++ Line_Comparision_Program/Line_Comparision_Program/Program.cs
@@ -4,7 +4,7 @@ namespace Line_Comparision_Program
 {
     class program
     {
-        static void Main(string[] args)
+        public void ComparingLines()
         {
             Console.WriteLine("Welcome to Line Comparision program \n");
             Console.WriteLine("Enter the coordinates of first line \n");
@@ -20,10 +20,10 @@ namespace Line_Comparision_Program
             Console.WriteLine("Enter the value of y2:");
             int y2 = int.Parse(Console.ReadLine());
 
-            double LengthL1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+             double LengthL1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine("Length of Line 1 : " + LengthL1);
-
-            Console.WriteLine("Enter the coordinates of second line \n");
+        
+           Console.WriteLine("Enter the coordinates of second line \n");
 
             Console.WriteLine("Enter the value of x3:");
             int x3 = int.Parse(Console.ReadLine());
@@ -40,23 +40,29 @@ namespace Line_Comparision_Program
             double LengthL2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
             Console.WriteLine("Length of Line 2 :" + LengthL2);
 
-            if (LengthL1 == LengthL2)
+            int lineStanding= LengthL1.CompareTo(LengthL2);
+
+            if (lineStanding > 0)
             {
-                Console.WriteLine(" Both Lines are equal");
+                Console.WriteLine("Line one is greater than line two");
             }
-            else if (LengthL1 > LengthL2)
+            else if (lineStanding < 0)
             {
-                Console.WriteLine("Line1 is greater than Line2");
+                Console.WriteLine("Line two is greater than line one");
             }
             else
             {
-                Console.WriteLine("Line2 is greater than Line1");
+                Console.WriteLine("Line one is equal to line two");
             }
-
         }
-
+           public static void Main(String[]args)
+           {
+           program compare=new program();
+            compare.ComparingLines();
+           }
     }
 }
+
 
 
 
